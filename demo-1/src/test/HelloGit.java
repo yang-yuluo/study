5b65b732d23fed6a0b142417b029bcccc59f5972
weidong <598772251@qq.com>
2020-04-22 11:19:04 +0800

    项目测试提交

--- demo-1/src/test/HelloGit.java
+++ demo-1/src/test/HelloGit.java
@@ -4,5 +4,9 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println(123456);
+
+        System.out.println("test-git peizhi");
+        System.out.println("test-git peizhi");
+        System.out.println("test-git peizhi");
     }
 }
